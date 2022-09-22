004dc69b5b1b8b6bdb463234461b1884a189b36a
nijat <nijat.de@gmail.com>
2022-09-22 21:02:59 +0200

    Part-4

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -53,4 +53,22 @@ public class ProjectController {
         return "redirect:/project/create";
     }
 
+    @GetMapping("/update/{projectCode}")
+    public String editProject(@PathVariable("projectCode") String projectCode, Model model) {
+
+        model.addAttribute("project", projectService.findById(projectCode));
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("managers", userService.findManagers());
+
+        return "/project/update";
+    }
+
+    @PostMapping("/update")
+    public String updateProject(ProjectDTO project) {
+
+        projectService.update(project);
+
+        return "redirect:/project/create";
+    }
+
 }
